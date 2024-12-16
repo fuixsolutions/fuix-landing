0085fe7ad208abe672dffd2f849129af87728c5f
Sabino Velasquez <sabinovelasquez@gmail.com>
2024-12-16 15:02:17 -0300

    fix: lang

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
     tailwind(),
     sitemap({
       i18n: {
-        defaultLocale: "en", // All urls that don't contain `fr` after `https://fuix.cl/` will be treated as default locale, i.e. `en`
+        defaultLocale: "root", // All urls that don't contain `fr` after `https://fuix.cl/` will be treated as default locale, i.e. `en`
         locales: {
           en: "en",
           es: "es",
@@ -35,7 +35,7 @@ export default defineConfig({
     }),
     starlight({
       title: "ScrewFast Docs",
-      defaultLocale: "es",
+      defaultLocale: "root",
       // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
       // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
       // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
